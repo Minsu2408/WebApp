700112500b0f68278d833b76b7be833a7256d6db
msu2408 <msu2408@gmail.com>
2026-04-15 17:53:58 +0900

    0415

---
+++ 0415/testhook/src/Counter.js
@@ -0,0 +1,12 @@
+// 1. Counter 컴포넌트
+function Counter() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
+      <button onClick={handleClick}>You pressed me {count} times</button>
+    </div>
+  );
+}
---
+++ 0415/testhook/src/Form.js
@@ -0,0 +1,12 @@
+// 4. Form 컴포넌트
+function Form() {
+  const [name, setName] = useState('Taylor');
+  const [age, setAge] = useState(42);
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setAge(age + 1)}>Increment age</button>
+      <p>Hello, {name}. You are {age}.</p>
+    </div>
+  );
+}
